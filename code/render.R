d73d974e63ba0aa1636670f99e1bc50f82ad4147
Kyle Roddick <76850901+KyleOfCanada@users.noreply.github.com>
2023-01-11 17:50:43 -0400

    adjust format and authors

--- code/render.R
+++ code/render.R
@@ -3,4 +3,4 @@ library(here)
 
 quarto_render(here("code", "3xTg-AD-serial-reversal.qmd"),
               output_file = here("docs",
-                                 str_c("report ", lubridate::today(), ".pdf")))
+                                 str_c("Serial reversal report ", lubridate::today(), ".pdf")))
