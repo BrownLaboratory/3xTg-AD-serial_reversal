2556d1e64b028be285e1fff89134a0a9ea0712fd
Kyle Roddick <76850901+KyleOfCanada@users.noreply.github.com>
2023-01-26 12:23:33 -0400

    re-arrange plots

--- code/render.R
+++ code/render.R
@@ -3,6 +3,6 @@ library(here)
 
 quarto_render(here("code", "3xTg-AD-serial-reversal.qmd"),
               output_file = here("docs",
-                                 stringr::str_c("Serial reversal report ",
+                                 paste0("Serial reversal report ",
                                        lubridate::today(),
                                        ".pdf")))
